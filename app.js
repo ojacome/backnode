7c95249aec4ef9b0fdcacf738300f94708eaff39
olmedo <olmedo,bdp@gmail.com>
2020-06-21 16:36:10 -0500

    configuracion del CORS

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ var article_routes = require('./routes/article')
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-//activar el CORS
+//activar el CORS IMPORTANTE para peticiones mediante ajax o angular
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 //Añadir prefijos a rutas
 app.use('/api/articles', article_routes);
